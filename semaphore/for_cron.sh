7fcae45a7ebc91a426282e3d54211c82d313978a
Andrew Cudzilo <andrew.cudzilo+github@gmail.com>
2017-06-24 13:26:38 -0400

    cleaned up logic in for loop to remove semaphores w/o pid as well.

--- semaphore/for_cron.sh
+++ semaphore/for_cron.sh
@@ -7,9 +7,11 @@ unset arr[0]; unset arr[1]; unset arr[2]; #depending on headers, etc, not semaph
 
 for i in ${arr[@]}; do
   pid=($("$ipcs_cmd" -s -i "$i" | awk -v RS="" '{print $36}'));
-  if [ ! -z "$pid" ] && [ "$pid" -ne "0" ] && [ ! -d /proc/"$pid" ]; then
+  if [ "$pid" ] && [ "$pid" -ne "0" ] && [ ! -d /proc/"$pid" ]; then
     $("$ipcrm_cmd") -s "$i"
     # log it if we wnat I suppose
     echo -e "$(date) - Removed semaphore with sem id $i due to terminated pid $pid" >> "$logfile"
+  else
+    $("$ipcrm_cmd") -s "$i" #no pid associated with semaphore, kill it.
   fi
 done
