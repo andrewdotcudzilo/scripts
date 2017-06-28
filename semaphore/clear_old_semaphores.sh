8cc63ea63be6791a5ec103534719745e39848b62
Andrew Cudzilo <andrew.cudzilo+github@gmail.com>
2017-06-28 16:08:31 -0400

    testing diff syntax for inc/dec counters

--- semaphore/clear_old_semaphores.sh
+++ semaphore/clear_old_semaphores.sh
@@ -44,4 +44,5 @@ do
   fi
   n=$((n+1))
 done < /proc/sysvipc/sem
+n=$((n--))
 echo "System semaphores=$n, and $d semaphores were deleted because there was no /proc reference to the PID and last mod time was >= 24hrs ago" >> $logfile
