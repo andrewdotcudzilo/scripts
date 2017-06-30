3b833b0c14688ec897a1567508d75fc796dbbc40
Andrew Cudzilo <andrew.cudzilo+github@gmail.com>
2017-06-29 22:27:49 -0400

    semi-finished

--- semaphore/monitor.sh
+++ semaphore/monitor.sh
@@ -4,31 +4,29 @@
 # of disconnects
 
 db="monitor.db"
-sleep=60
+sleep=10
 max_db_size=1000 #megs=1g
-cur_db_size=$(du -m ./"$db"|cut -f 1)
 
-if [ ! -f $db ]
-then
-  sqlite3 "$db" < monitor.sql
-fi
+if [ -f $db ]; then $(rm -fr ./"$db"); fi;
+if [ ! -f $db ]; then sqlite3 "$db" < monitor.sql; fi;
 
-thisIteration=1
 
-while [ 1 ]
+while(true)
 do
+
+  thisIteration=$(date +"%Y-%m-%d %H:%M:%S")
   n=0
   while read -r line
   do
     if [ $n -eq 0 ]; then n=$((n+1)); continue; fi; #skip first line of file
 
     read semid optime ctime <<< $(echo "$line" | awk '{print $2, $9, $10}')
-    if [ -z "$semid" ] || [ "$semid" -eq "0" ]; then n=$((n+1)); continue; fi; #null or owned by pid=0 stay away
+    if [ -z "$semid" ] || [ $semid -eq 0 ]; then n=$((n+1)); continue; fi; #null or owned by pid=0 stay away
 
     pid=$(ipcs -s -i "$semid" | awk 'FNR==9 {print $0}' | awk '{print $5}')
     if [ -z "$pid" ] || [ "$pid" -eq "0" ]; then n=$((n+1)); continue; fi; #null or pid=0 stay away
 
-    if [ -d /proc/"$pid"];
+    if [ -d /proc/"$pid" ];
     then
       cmd=$(cat /proc/"$pid"/cmdline)
       in_proc="Y"
@@ -37,8 +35,9 @@ do
       in_proc="N"
     fi;
 
-    echo "INSERT INTO monitor(semid, otime, ctime, pid, in_proc, cmd, iteration) VALUES ($semid, $otime, $ctime, $pid, $in_proc, $cmd, $thisIteration)"
+    sqlite3 monitor.db "INSERT INTO monitor(semid, otime, ctime, pid, in_proc, cmd, datetime) VALUES ($semid, $optime, $ctime, $pid, \"$in_proc\", \"$cmd\", \"$thisIteration\")"
+    echo "INSERT INTO monitor(semid, otime, ctime, pid, in_proc, cmd, datetime) VALUES ($semid, $optime, $ctime, $pid, $in_proc, $cmd, $thisIteration)"
     n=$((n+1))
-  done <<< /proc/sysvipc/sem
-  thisIteration=$((thisIteration+1))
+  done < /proc/sysvipc/sem
+  sleep "$sleep"
 done
