ebd5f35c7a8e802c70080375ef1d2a235d8376a7
Andrew Cudzilo <andrew.cudzilo+github@gmail.com>
2017-06-29 22:45:13 -0400

    sigh

--- semaphore/monitor.sql
+++ semaphore/monitor.sql
@@ -6,6 +6,6 @@ CREATE TABLE monitor(
   pid INT NOT NULL,
   in_proc CHAR(1) DEFAULT "N",
   cmd TEXT DEFAULT NULL,
-  iteration INT NOT NULL;
+  iteration INT NOT NULL
 );
 INSERT INTO monitor(id, semid, otime, ctime, pid, in_proc, cmd, iteration) VALUES(1, 0, 1, 1, 1, "N", NULL, 0);
