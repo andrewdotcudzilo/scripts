90afdc0c75683f8c4647b5ab2f74c2bbd44e46d8
Andrew Cudzilo <andrew.cudzilo+github@gmail.com>
2017-07-07 16:27:13 -0400

    added cron entry

--- cfenginefix/fix_cfengine.sh
+++ cfenginefix/fix_cfengine.sh
@@ -4,7 +4,7 @@ cfconf="/var/cfengine/inputs/update.conf"
 proxy="http://10.16.99.10:8888"
 confpath="http://debian.linux.ops.softcom.biz/update.conf"
 
-if [ ! -f  "$cfconf"] || [ -s "$cfconf" ]
+if [ ! -f  "$cfconf" ] || [ -s "$cfconf" ]
 then
   export http_proxy="$proxy"
   wget -O "$cfconf" "$confpath"
