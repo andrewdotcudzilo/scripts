bb818d3244c5f9b546bb3b4cad57cbfc18dc0d83
Andrew Cudzilo <andrew.cudzilo+github@gmail.com>
2017-07-07 16:14:40 -0400

    init commit to branch - fix cfengine

---
+++ cfenginefix/fix_cfengine.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cfconf="/var/cfengine/inputs/update.conf"
+proxy="http://10.16.99.10:8888"
+confpath="http://debian.linux.ops.softcom.biz/update.conf"
+
+if [ ! -f  "$cfconf"] || [ -s "$cfconf" ]
+then
+  export http_proxy="$proxy"
+  wget -O "$cfconf" "$confpath"
+  if [ $? -ne 0 ]; then exit 1; fi;
+  unset http_proxy
+fi
