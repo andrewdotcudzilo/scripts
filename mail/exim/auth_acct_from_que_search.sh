8cbea3e3e315df7004297fc8402c778ad1173b15
Andrew Cudzilo <andrew.cudzilo+github@gmail.com>
2017-10-26 12:00:31 -0400

    blah

--- mail/exim/auth_acct_from_que_search.sh
+++ mail/exim/auth_acct_from_que_search.sh
@@ -11,7 +11,8 @@ BASE_OUT='';
 
 while read -r line
 do
-  BASE_OUT=$(exim -Mvh "$line" | grep -o "Authenticated-user:.*" | sed -n -e 's/.*Authenticated\-user\:\_//p' | sed -e 's/\@[^\@]*$//')
+  BASE=$(exim -Mvh "$line" | grep -o "Authenticated-user:.*" | sed -n -e 's/.*Authenticated\-user\:\_//p' | sed -e 's/\@[^\@]*$//')
+  BASE_OUT=echo "$BASE"
 done < <(exim -bp | grep "$VARSTRING" | awk '{print $3}')
 
 echo "$BASE_OUT";
