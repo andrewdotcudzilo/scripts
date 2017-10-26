309e4ea87cde45701bb8ef33a176ea242f74526f
Andrew Cudzilo <andrew.cudzilo+github@gmail.com>
2017-10-26 12:18:42 -0400

    think I have it figured out

--- mail/exim/auth_acct_from_que_search.sh
+++ mail/exim/auth_acct_from_que_search.sh
@@ -6,13 +6,7 @@ function usage() {
 
 [[ $# -eq 0 ]] && usage
 
-BASE_OUT='';
-
-
 while read -r line
 do
-  BASE=$(exim -Mvh "$line" | grep -o "Authenticated-user:.*" | sed -n -e 's/.*Authenticated\-user\:\_//p' | sed -e 's/\@[^\@]*$//')
-  BASE_OUT=echo "$BASE"
-done < <(exim -bp | grep "$VARSTRING" | awk '{print $3}')
-
-echo "$BASE_OUT";
+  exim -Mvh "$line" | grep -o "Authenticated-user:.*" | sed -n -e 's/.*Authenticated\-user\:\_//p' | sed -e 's/\@[^\@]*$//'
+done < <(exim -bp | grep "$VARSTRING" | awk '{print $3}' | sed '/^$/d') | sort | uniq -c | sort -n
