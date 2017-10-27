1f393f9afaa815b1c8e7271c78212c4a33238d34
Andrew Cudzilo <andrew.cudzilo+github@gmail.com>
2017-10-27 10:17:19 -0400

    trying to bring in argument to script

--- mail/exim/exim_clean.sh
+++ mail/exim/exim_clean.sh
@@ -14,21 +14,22 @@ function usage() {
 
 [[ $# -eq 0 ]] && usage
 
+MIN_LIMIT=$1
 
 # mail older than 7 days
 find /var/spool/exim4/{input,msglog} -type f -mtime +7 -name 1\* | xargs rm -v
 
 # frozen and null sender
-exim -bp | grep '<>\|<"' | awk '{print $3}'' | xargs -n1 exim -Mrm
+exim -bp | grep '<>\|<"' | awk '{print $3}' | xargs -n1 exim -Mrm
 
-# run  exipick -b
-
+# run  exipick -b | awk ' $2 == "From:" {print $3}' | sort | uniq -c | sort -n to get number of
+# messages in que per from address, and delete them if from has more than N messages "
 
 EXIM_PATH=/var/spool/exim4/input/
 while read -r line1
 do
   grep -rl "$line1" "$EXIM_PATH" |  sed -e 's/^\.\///' -e 's/-[DH]$//' | sed 's/.*\///' | xargs -n1 exim -Mrm
-done < <( exipick -b | awk ' $2 == "From:" {print $3}' | sort | uniq -c| sort -n | awk '{if($1==$1+0 && $1>5)print $2}')
+done < <( exipick -b | awk ' $2 == "From:" {print $3}' | sort | uniq -c| sort -n | awk '{if($1==$1+0 && $1>"$MIN_LIMIT)print $2}')
 
 # restart exim
 /etc/init.d/exim4 stop; sleep 60; killall exim4; sleep 10; while(killall -9 exim4); do sleep 2; done; /etc/init.d/exim4 start
