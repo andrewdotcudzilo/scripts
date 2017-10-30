92cc072c695e14fdde1ecb5a757fd8672f9a0bfe
Andrew Cudzilo <andrew.cudzilo+github@gmail.com>
2017-10-30 10:56:32 -0400

    revised; still testing

--- mail/exim/all_qued_authd_mail.sh
+++ mail/exim/all_qued_authd_mail.sh
@@ -3,20 +3,28 @@
 while read -r line1
 do
   frun=1
+  gotres=0
   while read -r line2
   do
     res='';
     res=$(exim -Mvh "$line2" | grep -o "Authenticated-user:.*" | sed -n -e 's/.*Authenticated\-user\:\_//p' | sed 's/.$//')
     if [[ ! -z $res ]]
     then
-      if [[ $fun==1 ]]; then echo "$line1"; fi;
-      frun=0
+      if [[ $frun == 1 ]]
+      then
+        echo "Address: $line1"
+        frun=0
+      fi
       echo "$res"
-      echo "$line2"
+      gotres=1
     fi
   done < <(exim -bp | grep "$line1" | awk '{print $3}' | sed '/^$/d')
-  if [[ $frun==0 ]]; then echo "---"; fi;
+  if [[ $frun == 0 ]] && [[ $gotres > 0 ]]
+  then
+    echo " --- "
+    echo " "
+  fi
 done < <(
- exipick -b | awk ' $2 == "From:" {print $3}' | sort | uniq -c | sort -n | awk '{if($1==$1+0 && $1>4)print $2}') | \
+ exipick -b | awk ' $2 == "From:" {print $3}' | sort | uniq -c | sort -n | awk '{if($1==$1+0 && $1>4)print $2}' | \
  sed 's/^.\(.*\).$/\1/' | sed '/^$/d'
 )
