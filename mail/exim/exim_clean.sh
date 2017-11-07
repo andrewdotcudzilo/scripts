8237301db217b0b99aff54959de2ce389233f102
Andrew Cudzilo <andrew.cudzilo+github@gmail.com>
2017-11-07 10:46:52 -0800

    trying to find where the lag in second search is coming from

--- mail/exim/exim_clean.sh
+++ mail/exim/exim_clean.sh
@@ -58,7 +58,7 @@ do
     if [[ $DEL>0 ]]
     then
       echo "Removing $line emails"
-      grep -rl "$line" "$EXIM_PATH" |  sed -e 's/^\.\///' -e 's/-[DH]$//' | sed 's/.*\///' | xargs -n1 exim -Mrm
+      grep -rl "$line" "$EXIM_PATH" |  sed -e 's/^\.\///' -e 's/-[DHJ]$//' | sed 's/.*\///' | xargs -n1 exim -Mrm
     fi
   fi
 done < <(exipick -b | awk ' $2 == "From:" {print $3}' | sort | uniq -c| sort -n | awk '{if($1==$1+0 && $1>"$MIN_LIMIT")print $2}' | sed 's/^.\(.*\).$/\1/' | sed '/^\s*$/d' )
