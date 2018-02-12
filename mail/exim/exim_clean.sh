1a2f1d33aa1a404592de77b49d7f1c2476989e9c
Andrew Cudzilo <andrew.cudzilo+github@gmail.com>
2018-02-12 13:57:51 -0800

    simplified script, more restrictive

--- mail/exim/exim_clean.sh
+++ mail/exim/exim_clean.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 ### *Ultimate* exim mail cleanup script
 ### this is destructive, know what it does before you try using it.
-
-function usage() {
-  echo "Usage: $0 integer"
-  echo " "
-  echo "Where 'integer' is a minimum number of email messages stuck in que"
-  echo "from a specific address (ie: bingo@trade.bid)"
-  echo "If you don't know what to put, 5 is a decent number"
-  echo "This script is destructive so don't use it if you don't understand it."
-  exit 1
-}
-
-[[ $# -eq 0 ]] && usage
-MIN_LIMIT=$1
+FROM_MIN_LIMIT=4
 EXIM_PATH=/var/spool/exim4/input/
-MAX_LIMIT_PER_AUTH_SENDER=75
+MAX_LIM_AUTH_SENDER=25
 
 # mail older than 7 days
 find /var/spool/exim4/{input,msglog} -type f -mtime +7 -name 1\* | xargs rm -v
@@ -23,25 +11,24 @@ find /var/spool/exim4/{input,msglog} -type f -mtime +7 -name 1\* | xargs rm -v
 # frozen and null sender
 exim -bp | grep '<>\|<"' | awk '{print $3}' | xargs -n1 exim -Mrm
 
-# this will remove any email with an authenticated sender of $line, if the count is > 200
-# this is a temporary measure
+### search auth senders; if sender has >$MAX_LIM_AUTH_SENDER emails in que,
+# consider it spam and deletes all messages.
 while read -r line
 do
   if [[ ! -z  $line ]]
   then
-    echo "removing emails from auth user $line because they have >$MAX_LIMIT_PER_AUTH_SENDER emails in que and is likely spam"
-    echo "please follow up with abuse cases"
+    echo "Removing emails from Authenticated User: $line because count > $MAX_LIM_AUTH_SENDER"
+    echo "You should probably have already found/submitted abuse cases for these"
+    echo "accounts/emails"
     grep -rl "$line" "$EXIM_PATH" |  sed -e 's/^\.\///' | sed -e 's/-[DH]$//' | sed 's/.*\///' | xargs -n1 exim -Mrm
   fi
-done < <(grep -r "Authenticated-user:_.*" "$EXIM_PATH" | awk -F"_" {'print $2'} | awk -F"@" '{print $1 "@" $2}' \
-  | sort | uniq -c | sort -n | awk '{if($1==$1+0 && $1>"$MAX_LIMIT_PER_AUTH_SENDER")print $2}' | sed 's/^.\(.*\).$/\1/' | sed '/^\s*$/d')
 
+done < <(grep -r "Authenticated-user:_.*" "$EXIM_PATH" | awk -F"_" {'print $2'} | \
+  awk -F"@" '{print $1 "@" $2}' | sort | uniq -c | sort -n | \
+  awk '{if($1==$1+0 && $1>"$MAX_LIMIT_PER_AUTH_SENDER")print $2}' | sed '/^\s*$/d')
 
-
-## runs exipick looking for large amounts of qued messages "From" a specific email address
-# if they match the second conditional (regex) they are auto deleted
-# so far just .trade, .bid domain extensions.
-# this needs testing.
+### run exipick looking for large amount of messages with specific "From" email address
+# if they match our second conditional(s) they are deleted
 while read -r line
 do
   if [[ ! -z  $line ]]
@@ -49,10 +36,18 @@ do
     #i found some linux hosts arent handling or statements correctly in regex
     DEL=0;
 
-    if [[ $(echo "$line" | grep "\.trade$") ]]; then DEL=1;
-    elif [[ $(echo "$line" | grep "\.bid$") ]]; then DEL=1;
-    elif [[ $(echo "$line" | grep "\.returns\.groups\.yahoo\.com$") ]]; then DEL=1;
-    elif [[ $(echo "$line" | grep "\.win$") ]]; then DEL=1;
+    if [[ $(echo "$line" | grep "\.trade$") ]]
+      then DEL=1
+    elif [[ $(echo "$line" | grep "\.bid$") ]]
+      then DEL=1
+    elif [[ $(echo "$line" | grep "\.returns\.groups\.yahoo\.com$") ]]
+      then DEL=1
+    elif [[ $(echo "$line" | grep "\.win$") ]]
+      then DEL=1
+    elif [[ $(echo "$line" | grep "\.club$") ]]
+      then DEL=1
+    elif [[ $(echo "$line" | grep "support@caex.com") ]]
+      then DEL=1
     fi
 
     if [[ $DEL>0 ]]
@@ -61,7 +56,8 @@ do
       grep -rl "$line" "$EXIM_PATH" |  sed -e 's/^\.\///' -e 's/-[DHJ]$//' | sed 's/.*\///' | xargs -n1 exim -Mrm
     fi
   fi
-done < <(exipick -b | awk ' $2 == "From:" {print $3}' | sort | uniq -c| sort -n | awk '{if($1==$1+0 && $1>"$MIN_LIMIT")print $2}' | sed 's/^.\(.*\).$/\1/' | sed '/^\s*$/d' )
+done < <(exipick -b | awk ' $2 == "From:" {print $3}' | sort | uniq -c| sort -n | \
+  awk '{if($1==$1+0 && $1>"$FROM_MIN_LIMIT")print $2}' | sed 's/^.\(.*\).$/\1/' | sed '/^\s*$/d' )
 
 ### determined too agressivve
 # cannot be trusted at this point in time.
