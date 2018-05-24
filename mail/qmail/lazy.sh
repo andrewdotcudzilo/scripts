3eb38803609873f49c97a0d29d407d8861c4025d
Andrew Cudzilo <andrew.cudzilo+github@gmail.com>
2018-05-24 09:46:31 -0700

    adding qmail3

--- mail/qmail/lazy.sh
+++ mail/qmail/lazy.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-#/usr/local/script/qmqtool -d -f 'MAILER-DAEMON@xmail'
-#/usr/local/script/qmqtool -d -f 'rechnung@'
-#/usr/local/script/qmqtool -d -f 'OnlinePay'
-#/usr/local/script/qmqtool -d -f 'user@mywebsite.myhosting.com'
-#/usr/local/script/qmqtool -d -f 'bounce'
+/usr/local/script/qmqtool -d -f 'MAILER-DAEMON@xmail'
+/usr/local/script/qmqtool -d -f 'rechnung@'
+/usr/local/script/qmqtool -d -f 'OnlinePay'
+/usr/local/script/qmqtool -d -f 'user@mywebsite.myhosting.com'
+/usr/local/script/qmqtool -d -f 'bounce'
 
 while read -r line
 do
-  echo "$line"
+  del=(awk '{if($1==$1+0 && $1>30)print $2}')
+  if [ ! -z "$del" ]
+  then
+    echo 'deleting emails matching "$del" from qmail que'
+    /usr/local/script/qmqtool -d -f "$del"
+  fi
 done < <(/usr/local/script/qmqtool -l | awk '$2 == "Sender:" {print $3}' | sort | uniq -c | sort -n)
