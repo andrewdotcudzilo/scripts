43c760e7b14de1f8a3419959539731b0ccc79e55
Andrew Cudzilo <andrew.cudzilo+github@gmail.com>
2018-06-07 14:14:48 -0400

    added common spam removers

--- mail/qmail/lazy.sh
+++ mail/qmail/lazy.sh
@@ -17,6 +17,11 @@ done < <(/usr/bin/curl http://xsmtpsupport.mail2web.com/blacklists/local_sender_
 
 while read -r line
 do
+  /usr/local/script/qmqtool -d -f "$line"
+done < <(/usr/local/script/qmqtool -l | awk '$2 == "Sender:" {print $3}' | sort | uniq -c | sort -n | grep '\.trade\|\.win\|\.bid\|\.club' | awk '{print $2}')
+
+while read -r line
+do
   del=$(/usr/bin/awk '{if($1==$1+0 && $1>100)print $2}')
   if [ ! -z "$del" ]
   then
