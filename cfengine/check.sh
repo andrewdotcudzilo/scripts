528196afa022435c02b05624f6d3ddc180c187d0
Andrew Cudzilo <andrew.cudzilo+github@gmail.com>
2018-07-11 01:40:02 +0000

    adding cfegine scripts

---
+++ cfengine/check.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DEB_PATH=/var/lib/cfengine/inputs
+RH_PATH=/var/cfengine/inputs
+PATH=""
+
+if [ -d "$DEB_PATH" ]; then PATH=$DEB_PATH;
+elif [ -d "$RH_PATH" ]; then PATH=$RH_PATH;
+else exit 1;
+fi;
+
+if [ ! -f "$PATH/update.conf" ]
+then
+	echo "$PATH/update.conf not found, getting"
+	export http_proxy="http://10.16.99.10"
+	wget -O "$PATH/update.conf" http://debian.linux.ops.softcom.biz/update.conf
+	unset http_proxy
+fi
+
+cfagent --no-splay
+
+
+
---
+++ cfengine/remote.ex
@@ -0,0 +1,16 @@
+#!/usr/bin/expect -f
+set pass [lrange $argv 0 0]
+set server [lrange $argv 1 1]
+set name [lrange $argv 2 2]
+set prompt {$ }
+
+spawn ssh $name@$server 
+#match_max 1000000
+#set timeout 5
+expect "*?re" { send "yes\r" }
+expect "?assword:*" { send "$pass\r" }
+sleep 10
+expect "$prompt" { send "sudo ls -1 /usr/local/pem/vhosts | grep -v txt\r"}
+expect "*andrewcu:*" { send "$pass\r" }
+expect "$prompt"
+puts $expect_out(buffer)
