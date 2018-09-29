dbc9ccb1ee7f537132645947cf67deb005aa07b6
Andrew Cudzilo <andrew.cudzilo+github@gmail.com>
2018-09-29 03:33:56 +0000

    test4

--- cfengine/check.sh
+++ cfengine/check.sh
@@ -13,13 +13,13 @@ fi;
 
 if [ ! -f "$PATH/update.conf" ]
 then
-	echo "$PATH/update.conf not found, getting"
+	/bin/echo "$PATH/update.conf not found, getting"
 	export http_proxy="http://10.16.99.10"
 	/usr/bin/wget -O "$PATH/update.conf" http://debian.linux.ops.softcom.biz/update.conf
 	unset http_proxy
 fi
 
-CFRUNNING=$(ps aux|grep "/usr/sbin/cfexecd"|grep -v grep|wc -l)
+CFRUNNING=$(/bin/ps aux | /bin/grep "/usr/sbin/cfexecd"| /bin/grep -v grep| /usr/bin/wc -l)
 if [ "$CFRUNNING" -eq "0" ]
 then
 	/usr/bin/killall -9 cfagent
