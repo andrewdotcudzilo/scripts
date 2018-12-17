1324474b171de9bad3cbcaf48c0b01d8fa29bfb2
Andrew Cudzilo <andrew.cudzilo+github@gmail.com>
2018-12-17 22:16:09 +0000

    some checks to xweb poc..sh

--- migration/xweb/poc.sh
+++ migration/xweb/poc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #set -x
 ZONE_PATH=./customer_pz
+BACKUP_FILE=$HOME/customer_pz_$(date).tar.gz
 SERIALDATE=2018123101
 
 if [ ! -f $1 ]; then echo "invalid map file"; exit 1; fi;
@@ -15,7 +16,11 @@ for i in ${rows[@]}; do
 done;
 SEDSTR=" $SEDSTR -e \"s/.*serial number/\t\t\t$SERIALDATE ; serial number/g\"";
 
-for filename in $ZONE_PATH/*; do
+tar -zcvf $ZONEPATH $BACKUP_FILE;
+if [ ! $? -eq 0 ]; then echo "error with backup file $BACKUP_FILE...exiting"; exit 1; fi;
+
+for filename in $ZONE_PATH/*.dns; do
+	if [ ! -f $filename ] echo "$filename no longer exists, skipping."; continue; fi;
 	echo "$filename"
 	eval "$SEDSTR $filename"
 done;
