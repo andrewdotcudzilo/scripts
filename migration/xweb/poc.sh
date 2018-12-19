87bc62d578e09508aa9724ff54e0ed458883af83
Andrew Cudzilo <andrew.cudzilo+github@gmail.com>
2018-12-19 17:03:52 +0000

    super script no testing

--- migration/xweb/poc.sh
+++ migration/xweb/poc.sh
@@ -1,68 +1,113 @@
 #!/bin/bash
 
-#set -x
+#constants
 ZONE_PATH=./customer_pz
 BACKUP_FILE=$HOME/customer_pz_$(date '+%Y%m%d%H%M%S').tar.gz
 SERIALDATE=$(date '+%Y%m%d'"01");
+VERBOSE=0
+EXCLUDE=0
+EXCLUDE_FILE='';
+MAP_FILE='';
+BAIL=0;
+DNS_FILE_EXT=.dns
 
-#check for mapping file
-if [ ! -f $1 ]; then echo "invalid map file"; exit 1; fi;
+usage() {
+ echo "-------------------------------------"
+ echo `basename $0`;
+ echo "-h :: display this help"
+ echo "-b <file> - path to backup file"
+ echo "-d <string> - serial date / num field for update"
+ echo "-m <file> - ip mapping file"
+ echo "-v - enable verbose mode"
+ echo "-x <file> - path to exclusion file"
+ echo "-z <path> - path to zone files"
+}
 
-#check for zone path/dir
-if [ ! -d $ZONE_PATH ]; then echo "zone directory not found, exiting..."; exit 1; fi;
+# handle parameters/ops
+while getopts "z:b:x:d:vh" opt; do
+ case $opt in
+ h) usage; exit 0 ;;
+ b) BACKUP_FILE=$OPTARG ;;
+ d) SERIALDATE=$OPTARG ;;
+ m) MAP_FILE=$OPTARG ;;
+ x) EXCLUDE=1; EXCLUDE_FILE=$OPTARG ;;
+ z) ZONE_PATH=$OPTARG ;;
+ v) VERBOSE=1 ;;
+ \?) echo "invalid option triggered" >&2 ;;
+ esac
+done
+exit 0;
 
-#check for exclusions
-EXCLUDE=0;
-if [ ! -z $2 ]; then
-	if [ ! -f $2 ]; then echo "exclude file not found, exiting..."; exit 1; 
-	else EXCLUDE=1; 
-	fi;
-fi;
+# sanity checks
+if [ ! -f $MAP_FILE ]; then echo "invalid ip mapping file: $MAP_FILE "; BAIL=1; fi;
+if [ ! -d $ZONE_PATH ]; then echo "invalid dns zone directory not found: $ZONE_PATH "; BAIL=1; fi;
+if [ $EXCLUDE -gt 0 ]; then
+ if [ ! -f $EXCLUDE_PATH ]; then echo "invalid dns domain exclusion list file: $EXCLUDE_PATH "; BAIL=1; fi;
+fi
+# crit bail
+if [ $BAIL -gt 0 ]; then exit 1; fi;
 
-readarray rows < $1;
-MAP_COUNT="${#rows[@]}";
-COUNT=1;
-
-SEDSTR="sed -i"
-for i in ${rows[@]}; do
-	IFS=',' read -ra DATA <<< ${i[@]};
-	SEDSTR=" $SEDSTR -e s/${DATA[0]}/${DATA[1]}/g";
-	echo -ne "Buiding map file - item $COUNT of $MAP_COUNT ...\r";
-	COUNT=$((COUNT+1));
+# reading in ip mapping
+readarray maps < $MAP_FILE
+map_count="${#maps[@]}";
+map_i=1;
+echo " Building ip mapping list for zone file updates ... "
+SED_CMD="sed -i"
+for i in ${maps[@]}; do
+  IFS',' read -ra DATA <<< ${i[@]};
+  SED_CMD=" $SED_CMD -e s/${DATA[0]}/${DATA[1]}/g";
+  if [ $VERBOSE ]; then echo -ne " Building ip mapping list for zone file updates ... $map_i / $map_count \r"; fi;
+  map_i=$((map_i+1));
 done;
-SEDSTR=" $SEDSTR -e \"s/.*serial number/\t\t\t$SERIALDATE ; serial number/g\"";
+SED_CMD=" $SED_CMD -e  \"s/.*serial number/\t\t\t$SERIALDATE ; serial number/g\"";
 
-#back customer_pz
+# backup the source dns zone directory/files
 echo "";
-echo "Backing up $ZONE_PATH ...";
+echo " Backing up $ZONE_PATH "
 tar -czf $BACKUP_FILE $ZONE_PATH;
 if [ ! $? -eq 0 ]; then echo "error with backup file $BACKUP_FILE...exiting"; exit 1; fi;
 
+# get the source list of files to update
+echo " Building list of zone files to update ... ";
+files=($(ls -1 $ZONE_PATH| grep "$DNS_FILE_EXT"));
+file_count=${#files[@]};
+file_i=1;
+echo " Currently $file_count files will be checked ... ";
 
-# generate working list of files to update
-files=($(ls -1 $ZONE_PATH|grep ".dns"));
-FILE_COUNT="${#files[@]}";
-echo "$FILE_COUNT files to be updated...";
+# handle dns exclusion file as list of domain names.
+if [ $EXCLUDE -gt 0 ]; then
+  echo "";
+  echo " Exclusion file provided, checking domains for exclusion ";
+  readarray delete < $EXCLUDE_FILE;
+  delete_count=${#delete[@]};
+  delete_i=1;
+  if [ $VERBOSE ]; then 
+    echo " $delete_count exclusions loaded from file ";
+    echo " this will take a while ...";
+  fi;
 
-if [ $EXCLUDE -eq 1 ]
-then
-	echo "checking files to be removed from updates";
-	readarray delete < $2;
-
-	for i in ${!files[@]}; do
-		echo "checking: "${files[$i]};
-		for j in ${!delete[@]}; do
-			VAL="${delete[$j]%?}.dns";
-			if [ "${files[$i]}" = "$VAL" ]; then echo "match ${files[$i]} $VAL"; files=("${files[@]/$VAL}"); fi;
-		done;
-	done;
+  for i in ${!files[@]}: do
+    filebasename=$(basename ${files[$i]} .dns);
+    filename=${files[$i]};
+#    if [ $VERBOSE ]; then echo " checking $filebasename for exclusion"; fi;
+    for j in ${!delete[@]}; do
+      if [ "$filebasename" = ${delete[$j]} ]; then
+        if [ $VERBOSE ]; then echo "Removing $filebasename from dns updates"; fi;
+        files=("${files[@]/$filename}");
+      fi;
+    done;
+  done;
 fi
 
-for file in ${files[@]}; do echo "$file" >> out.log; done;
-
+# stop for now
 exit 0;
+
+file_count=${#files[@]};
+file_i=1;
+# finally do the updates
 for file in ${files[@]}; do
-	if [ ! -f $ZONE_PATH/$file ]; then echo "$file no longer exists, skipping."; continue; fi;
-	echo "$ZONE_PATH/$file";
-	eval "$SEDSTR $ZONE_PATH/$file";
+  if [ ! -f $ZONE_PATH/$file ]; then echo "$ZONE_FILE/$file no longer exists, skip for now but you should check this."; continue; fi;
+  if [ $VERBOSE ]; then echo "Updating $ZONE_PATH/$file ... $file_i / $file_count "; fi;
+  file=$((file_i+1));
+  eval "$SED_CMD $ZONE_PATH/$file";
 done;
