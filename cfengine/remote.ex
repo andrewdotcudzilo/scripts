314dc9c92175f191a792b6b5f0d19d6f0a72528b
Andrew Cudzilo <andrew.cudzilo+github@gmail.com>
2018-12-26 15:26:28 +0000

    old cfengine stuff

--- cfengine/remote.ex
+++ cfengine/remote.ex
@@ -4,13 +4,14 @@ set server [lrange $argv 1 1]
 set name [lrange $argv 2 2]
 set prompt {$ }
 
-spawn ssh $name@$server 
+spawn ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $name@$server 
 #match_max 1000000
 #set timeout 5
-expect "*?re" { send "yes\r" }
 expect "?assword:*" { send "$pass\r" }
-sleep 10
 expect "$prompt" { send "curl -s -k https://raw.githubusercontent.com/andrewdotcudzilo/scripts/master/cfengine/check.sh | sudo bash\r" }
+
 expect "*andrewcu:*" { send "$pass\r" }
+expect "$prompt"
+#sleep 15
 expect "$prompt"
 puts $expect_out(buffer)
