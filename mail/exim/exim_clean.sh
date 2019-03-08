7a1d8e3c0a6061657aba0131deb73e898169b0ef
Andrew Cudzilo <andrew.cudzilo+git@gmail.com>
2019-03-08 20:48:37 +0000

    removing softcom

--- mail/exim/exim_clean.sh
+++ mail/exim/exim_clean.sh
@@ -3,7 +3,7 @@
 ### this is destructive, know what it does before you try using it.
 FROM_MIN_LIMIT=4
 EXIM_PATH=/var/spool/exim4/input/
-MAX_LIM_AUTH_SENDER=25
+MAX_LIM_AUTH_SENDER=75
 
 # mail older than 7 days
 find /var/spool/exim4/{input,msglog} -type f -mtime +7 -name 1\* | xargs rm -v
@@ -13,19 +13,19 @@ exim -bp | grep '<>\|<"' | awk '{print $3}' | xargs -n1 exim -Mrm
 
 ### search auth senders; if sender has >$MAX_LIM_AUTH_SENDER emails in que,
 # consider it spam and deletes all messages.
-#while read -r line
-#do
-#  if [[ ! -z  $line ]]
-#  then
-#    echo "Removing emails from Authenticated User: $line because count > $MAX_LIM_AUTH_SENDER"
-#    echo "You should probably have already found/submitted abuse cases for these"
-#    echo "accounts/emails"
-#    grep -rl "$line" "$EXIM_PATH" |  sed -e 's@.*/@@' | sed -e 's/-[DHJ]$//' |  xargs -n1 exim -Mrm
-#  fi
+while read -r line
+do
+  if [[ ! -z  $line ]]
+  then
+    echo "Removing emails from Authenticated User: $line because count > $MAX_LIM_AUTH_SENDER"
+    echo "You should probably have already found/submitted abuse cases for these"
+    echo "accounts/emails"
+    grep -rl "$line" "$EXIM_PATH" |  sed -e 's@.*/@@' | sed -e 's/-[DHJ]$//' |  xargs -n1 exim -Mrm
+  fi
 
-#done < <(grep -r "Authenticated-user:_.*" "$EXIM_PATH" 2>&1 | awk -F"_" {'print $2'} | \
-#  awk -F"@" '{print $1 "@" $2}' | sort | uniq -c | sort -n | \
-#  awk '{if($1==$1+0 && $1>"$MAX_LIMIT_PER_AUTH_SENDER")print $2}' | sed '/^\s*$/d')
+done < <(grep -r "Authenticated-user:_.*" "$EXIM_PATH" 2>&1 | awk -F"_" {'print $2'} | \
+  awk -F"@" '{print $1 "@" $2}' | sort | uniq -c | sort -n | \
+  awk '{if($1==$1+0 && $1>"$MAX_LIMIT_PER_AUTH_SENDER")print $2}' | sed '/^\s*$/d')
 
 ### run exipick looking for large amount of messages with specific "From" email address
 # if they match our second conditional(s) they are deleted
@@ -48,6 +48,11 @@ do
       then DEL=1
     elif [[ $(echo "$line" | grep "support@caex.com") ]]
       then DEL=1
+    elif [[ $(echo "$line" | grep "softcom.com") ]]
+      then DEL=1
+    elif [[ $(echo "$line" | grep "softcom.biz") ]]
+      then DEL=1
+
     fi
 
     if [[ $DEL>0 ]]
