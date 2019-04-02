f9384c8af8a3d0762e2ce602bb299463b7abc430
Andrew Cudzilo <andrew.cudzilo+git@gmail.com>
2019-04-02 14:35:56 +0000

    less dangerous

--- mail/qmail/lazy.sh
+++ mail/qmail/lazy.sh
@@ -18,14 +18,14 @@ do
   /usr/local/script/qmqtool -d -f "$line"
 done < <(/usr/local/script/qmqtool -l | awk '$2 == "Sender:" {print $3}' | sort | uniq -c | sort -n | grep '\.trade\|\.win\|\.bid\|\.club' | awk '{print $2}')
 
-while read -r line
-do
-  del=$(/usr/bin/awk '{if($1==$1+0 && $1>100)print $2}')
-  if [ ! -z "$del" ]
-  then
-    /bin/echo "deleting emails matching $del from qmail que"
-    /usr/local/script/qmqtool -d -f "$del"
-  fi
-done < <(/usr/local/script/qmqtool -l | /usr/bin/awk '$2 == "Sender:" {print $3}' | /usr/bin/sort | /usr/bin/uniq -c | /usr/bin/sort -n)
+#while read -r line
+#do
+#  del=$(/usr/bin/awk '{if($1==$1+0 && $1>100)print $2}')
+#  if [ ! -z "$del" ]
+#  then
+#    /bin/echo "deleting emails matching $del from qmail que"
+#    /usr/local/script/qmqtool -d -f "$del"
+#  fi
+#done < <(/usr/local/script/qmqtool -l | /usr/bin/awk '$2 == "Sender:" {print $3}' | /usr/bin/sort | /usr/bin/uniq -c | /usr/bin/sort -n)
 
 /etc/init.d/qmail restart
